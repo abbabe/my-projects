629bbc8cf0dbe8eac35f6dc0449ecd62f45659fb
abbabe <dagistan18@gmail.com>
2022-08-10 22:58:09 +0300

    firstWebPage

---
+++ first-web-page/web.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/")
+def home():
+    return render_template("index.html/home")
+
+@app.route("/about", methods=['GET'])
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True,port=3000)
